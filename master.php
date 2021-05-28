b18725600c526f47a1d5440cb445a346c6e75fc6
Mihir2609 <84737914+Mihir2609@users.noreply.github.com>
2021-05-28 20:58:42 +0530

    Add files via upload

---
+++ master.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<body>
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>TEAMEX RETAIL LTD</title>
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+</body>
+<div id="menu-wrapper">
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><a href="home.php">Homepage</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact</a></li>
+		</ul>
+	</div>
+	<!-- end #menu --> 
+</div>
+<div id="header-wrapper">
+	<div id="header">
+		<div id="logo">
+			<h1><a href="#">TEAMEX RETAIL LTD</a></h1>
+			<p>Template developed by <a href="http://www.freecsstemplates.org">Mihir modi</a></p>
+		</div>
+	</div>
+</div>
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+            <div><img src="Basic Minimalism.jpg" width="920" height="300" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Welcome to TEAMEX RETAIL LTD </a></h2>
+				
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+					Teamex Retail Limited has been operating its functions since 2013 A.D. and had been started with just three products. Now we have range of around 80 prime quality products with prominent in various sectors like Health Care, Personal Care, Home Care, Lifestyle, Agriculture and Educational.
+
+Our products comply with the environmental policies which we strongly believe in respecting. Teamex Family is aiming to reach every house hold to create a platform for the people who wants to get financial freedom in their life. Teamex provides opportunity for the financial freedom, those are work hardly in their journey of life.
+
+The marketing plan and the management that has been able to deliver such a rewarding and sustainable system. Teamex is an ISO 9001-2015 certified company and believes in world class service levels to all its customers. Teamex has been enrolled as a member of Federation of Indian Direct Selling industries (FIDSI), which is the sole representative of Indian direct selling companies.
+
+Our Head Office is based in Ahmedabad, Gujarat in a state of the art building with conference halls, warehouse and other related amenities. A team of young professionals take care of the administration and customer support activities. We have a very effective customer grievance redressal system.
+
+
+				</div>
+			</div>
+			
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+		<div id="sidebar">
+                    <h2>News</h2><!-- comment -->
+                    <marquee>Welcome To TEAMEX RETAIL LTD</marquee>
+			<ul>
+				
+								<li>
+					<h2>OUR SECTORS</h2>
+					<ul>
+						<li><a href="#">HEALTH CARE</a></li>
+						<li><a href="#">HAIR CARE</a></li>
+						<li><a href="#">ORGANIC FARMING</a></li>
+						<li><a href="#">BODY CARE</a></li>
+						<li><a href="#">AUTO CARE</a></li>
+						<li><a href="#">FACE CARE</a></li>
+                                                <li><a href="#">HOME CARE</a></li>
+                                                <li><a href="#">LIFESTYLE</a></li>
+					</ul>
+				</li>
+				
+			</ul>
+		</div>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<div id="footer">
+	<p>2021 Developed by <a href="http://www.freecsstemplates.org/">Mihir Modi</a>.</p>
+</div>
+<!-- end #footer -->
+
+
